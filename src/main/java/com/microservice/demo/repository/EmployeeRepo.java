b73c3a0edb975c5ba7db98b04707ae3d4ccc7e79
Luthfy <luthfyaqilmahendra9@gmail.com>
2025-05-20 10:29:50 +0700

    create interface and employee class

---
+++ src/main/java/com/microservice/demo/repository/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package com.microservice.demo.repository;
+
+import com.microservice.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+}
